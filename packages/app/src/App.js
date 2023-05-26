4d48f0bd64c8ab24e553f965df9e3b108487ba7e
Alban Egidius <albanegidius12@gmail.com>
2023-05-26 18:25:53 +0300

    inserted the three buttons

--- packages/app/src/App.js
+++ packages/app/src/App.js
@@ -1,31 +1,38 @@
 import React from 'react'
-import { DataQuery } from '@dhis2/app-runtime'
-import i18n from '@dhis2/d2-i18n'
+import { Button, ButtonStrip } from "@dhis2/ui";
 import classes from './App.module.css'
 
-const query = {
-    me: {
-        resource: 'me',
-    },
-}
+// const query = {
+//     me: {
+//         resource: 'me',
+//     },
+// }
 
 const MyApp = () => (
-    <div className={classes.container}>
-        <DataQuery query={query}>
-            {({ error, loading, data }) => {
-                if (error) return <span>ERROR</span>
-                if (loading) return <span>...</span>
-                return (
-                    <>
-                        <h1>
-                            {i18n.t('Hello {{name}}', { name: data.me.name })}
-                        </h1>
-                        <h3>{i18n.t('Welcome to DHIS2!')}</h3>
-                    </>
-                )
-            }}
-        </DataQuery>
+    <div>
+        <div
+    style={{
+        border: '1px solid #c4c9cc',
+        padding: 8,
+        paddingTop: 130,
+        width: '100%'
+    }}
+>
+    <ButtonStrip>
+    <Button name="Primary button"  primary value="default">
+    Update
+    </Button>
+        <Button>
+            Options
+        </Button>
+        <Button>
+            Download
+        </Button>
+    </ButtonStrip>
+
+        </div>
     </div>
+
 )
 
 export default MyApp
