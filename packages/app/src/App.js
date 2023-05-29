9ce7f8b884141954479ff5777a1ba76d1bcaec6b
Alban Egidius <albanegidius12@gmail.com>
2023-05-29 12:59:00 +0300

    VisualizationType

--- packages/app/src/App.js
+++ packages/app/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Button, ButtonStrip } from "@dhis2/ui";
+import { Button, ButtonStrip, DropdownButton, FlyoutMenu, MenuItem } from "@dhis2/ui";
 import classes from './App.module.css'
 
 // const query = {
@@ -22,12 +22,21 @@ const MyApp = () => (
     <Button name="Primary button"  primary value="default">
     Update
     </Button>
-        <Button>
+        <Button
+        >
             Options
         </Button>
         <Button>
             Download
         </Button>
+        
+        <DropdownButton
+            component={<FlyoutMenu><MenuItem label="Line Graph" /><MenuItem label="Bar Chart" /><MenuItem label="Stacked Bar Chart" /></FlyoutMenu>}
+            name="buttonName"
+            value="buttonValue"
+        >
+            Visualization Type
+        </DropdownButton>
     </ButtonStrip>
 
         </div>
